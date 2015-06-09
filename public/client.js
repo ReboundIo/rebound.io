186c8a2cf07fef15568835092054acec13997cd4
stigcoder04 <stigcoder@gmail.com>
2015-06-08 22:15:09 -0700

    Tried to Fix Themes.

--- public/client.js
+++ public/client.js
@@ -2,6 +2,8 @@ var mySocket;
 var roomNum;
 var theme;
 var unreadMessages = 0;
+var i, link_tag;
+var clientStyle;
 defaultTheme();
 
 $(function() {
@@ -381,4 +383,20 @@ function updateTitle() {
     else {
         document.title = '[' + unreadMessages + '] Rebound Chat';
     }
+}
+
+//Script for changing the theme.
+
+function switch_style(css_title) {
+  var i, link_tag ;
+  for (i = 0, link_tag = document.getElementsByTagName("link") ;
+    i < link_tag.length ; i++ ) {
+    if ((link_tag[i].rel.indexOf( "stylesheet" ) != -1) &&
+      link_tag[i].title) {
+      link_tag[i].disabled = true ;
+      if (link_tag[i].title == css_title) {
+        link_tag[i].disabled = false ;
+      }
+    }
+  }
 }
