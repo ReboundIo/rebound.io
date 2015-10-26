225478299cf765630ca340d7516e557c3d632bf0
Aryan Jha <aryanjha@live.com>
2015-10-25 21:25:44 -0700

    Annnother update

--- public/client.js
+++ public/client.js
@@ -3,8 +3,8 @@ var unreadMessages = 0;
 var i, link_tag;
 var themeSelect;
 //checkNameColor();
-var bannedWords = ["shit", "SHIT", "fuck", "FUCK", "damn", "DAMN", "bitch", "BITCH", "dick", "DICK", "cock", "COCK", "pussy", "PUSSY", "asshole", "ASSHOLE", "fag", "FAG", "bastard", "BASTARD", "hoda", "HODA"];
-var replaceWords = ["potatoes", "POTATOES", "hug", "HUG", "throw tomatoes at", "THROW TOMATOES AT", "illuminati", "ILLUMINATI", "nose", "NOSE", "candy", "CANDY", "music box", "MUSIC BOX", "amazing person", "AMAZING PERSON", "gift to the world", "GIFT TO THE WORLD", "one-does-not-simply-walk-into-hordor", "naturally-a-disaster"];
+var bannedWords = ["shit", "SHIT", "fuck", "FUCK", "damn", "DAMN", "bitch", "BITCH", "dick", "DICK", "cock", "COCK", "pussy", "PUSSY", "asshole", "ASSHOLE", "fag", "FAG", "bastard", "BASTARD", "ass", "ASS", "hoda", "HODA"];
+var replaceWords = ["potatoes", "POTATOES", "hug", "HUG", "throw tomatoes at", "THROW TOMATOES AT", "illuminati", "ILLUMINATI", "nose", "NOSE", "candy", "CANDY", "music box", "MUSIC BOX", "avocado", "AVOCADO", "amazing person", "AMAZING PERSON", "gift to the world", "GIFT TO THE WORLD", "avocado", "AVOCADO", "one-does-not-simply-walk-into-hordor", "naturally-a-disaster"];
 
 var messageAlert = new Audio('messagealert.mp3');
 
@@ -346,7 +346,7 @@ $(function() {
           .append($usernameDiv, $messageBodyDiv);
         }
 
-        for (i = 0;i < 22;i++) {
+        for (i = 0;i < 24;i++) {
 
           if (data.message.indexOf(bannedWords[i]) != -1) {
             $messageBodyDiv = $('<span class="messageBody">')
